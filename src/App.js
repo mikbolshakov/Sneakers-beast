cf54af2bfebfade07735eca12b68c4805f584d2c
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2022-09-13 16:24:24 +0300

    path no '/'

--- src/App.js
+++ src/App.js
@@ -131,7 +131,7 @@ function App() {
 
         <Routes>
           <Route
-            path="/"
+            path=""
             element={
               <Home
                 items={items}
@@ -146,9 +146,9 @@ function App() {
             }
           />
 
-          <Route path="/favorites" element={<Favorites />} />
+          <Route path="favorites" element={<Favorites />} />
 
-          <Route path="/orders" element={<Orders />} />
+          <Route path="orders" element={<Orders />} />
         </Routes>
       </div>
     </AppContext.Provider>
